c3c8ff9ae48600f0b0359e6a4acd92b73ffea237
gcoello <gcoello@institutmvm.cat>
2021-01-21 13:16:56 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+def main():
+    i = 1
+    sum = 0
+    num = 0
+    while i <= 10:
+        input("Introdueix un nombre:")
+        i += 1
+        sum = num + i
+
+    print("La media aritmetica és:", )
+if __name__ == '__main__':
+    main()
+
---
+++ main1.py
@@ -0,0 +1,5 @@
+def main():
+    palabra = input("Escriu una paraula:")
+    print((palabra)[::-1])
+if __name__ == '__main__':
+    main()
