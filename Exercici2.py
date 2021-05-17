92e73f05fb4c0ba434e4dacb2ad3cccbf4c69624
Daniel Coello <60615715+Dcoello12@users.noreply.github.com>
2021-05-17 10:47:36 +0200

    Add files via upload

---
+++ Exercici2.py
@@ -0,0 +1,8 @@
+import functions as f
+
+def main():
+    f.inici()
+    f.fileadd("files/text.txt")
+
+if __name__ == "__main__":
+    main()
---
+++ Exercici3.py
@@ -0,0 +1,8 @@
+import functions as f
+
+def main():
+    f.inici()
+    f.menu()
+
+if __name__ == "__main__":
+    main()
